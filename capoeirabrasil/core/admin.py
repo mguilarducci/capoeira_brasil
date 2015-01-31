d44f834a3a765040616527bc96022807c6a4da29
Matheus Guilarducci <mguilarducci@gmail.com>
2015-01-31 17:48:01 -0200

    habilitar admin para capoeirista

--- capoeirabrasil/core/admin.py
+++ capoeirabrasil/core/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# coding: utf-8
 
-# Register your models here.
+from django.contrib import admin
+from capoeirabrasil.core.models import Capoeirista
+
+admin.site.register(Capoeirista)
+
